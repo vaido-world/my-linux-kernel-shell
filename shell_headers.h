6d4e13255b499cfdba4e0128a004d4c1119be83a
Feldwor <BoQsc@users.noreply.github.com>
2021-10-03 21:09:01 +0300

    Update shell_headers.h

--- shell_headers.h
+++ shell_headers.h
@@ -1,1 +1,10 @@
- 
+#ifndef SHELL_HEADER_H
+#define SHELL_HEADER_H
+
+#____Standard Header____
+#include <stdio.h>
+
+#include <unistd.h>
+#         unistd.h header file provides access to the POSIX operating system API.
+
+#endif
