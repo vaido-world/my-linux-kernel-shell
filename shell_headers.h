43541c947378e7b5abb82ade6e09f0618cdd1530
Feldwor <BoQsc@users.noreply.github.com>
2021-10-03 21:10:36 +0300

    Update shell_headers.h

--- shell_headers.h
+++ shell_headers.h
@@ -3,6 +3,7 @@
 
 #____Standard Header____
 #include <stdio.h>
+#         stdio.h header file provides many standard library functions for file input and output. 
 
 #include <unistd.h>
 #         unistd.h header file provides access to the POSIX operating system API.
