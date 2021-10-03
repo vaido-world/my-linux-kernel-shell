9370b5f96dbe52b9e6b55ae40eecdc69cfd5e34a
Feldwor <BoQsc@users.noreply.github.com>
2021-10-03 21:15:46 +0300

    Update shell_headers.h

--- shell_headers.h
+++ shell_headers.h
@@ -2,12 +2,12 @@
 #define SHELL_HEADER_H
 
 #include <stdio.h>
-#         stdio.h header file provides many standard library functions for file input and output. 
+//        stdio.h header file provides many standard library functions for file input and output. 
 
 #include <unistd.h>
-#         unistd.h header file provides access to the POSIX operating system API.
+//        unistd.h header file provides access to the POSIX operating system API.
 
 #include <string.h>
-#         string.h header defines one variable type, one macro, and various functions for manipulating arrays of characters.
+//        string.h header defines one variable type, one macro, and various functions for manipulating arrays of characters.
 
 #endif
