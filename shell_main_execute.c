7ca645116af6d166f79ff603d181b42be255c2b7
BoQsc <BoQsc@users.noreply.github.com>
2021-10-06 11:28:26 +0300

    Update shell_main_execute.c

--- shell_main_execute.c
+++ shell_main_execute.c
@@ -15,7 +15,7 @@ int shell_execute(char **args)
 
   for (i = 0; i < count_shell_number_builtins(); i++) {
     if (strcmp(args[0], shell_builtin_command_list[i]) == 0) {
-      return (*shell_array_pointing_builtin_functions[i])(args); // Selects and executes from pointer list pointing to  builtin functions
+      return (*shell_array_pointing_builtin_functions[i])(args); // Selects and executes from pointer list pointing to builtin functions
     }
   }
 
