8835b3dd8bbe19ef863d505794cd8ca47e8165a6
BoQsc <BoQsc@users.noreply.github.com>
2021-10-06 13:13:42 +0300

    Update shell_main.c

--- shell_main.c
+++ shell_main.c
@@ -15,9 +15,9 @@
 /*
   Function Declarations for builtin shell commands:
  */
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
+int shell_builtin_command_cd(char **args);
+int shell_builtin_command_help(char **args);
+int shell_builtin_command_exit(char **args);
 
 /*
   List of builtin commands, followed by their corresponding functions.
@@ -38,9 +38,9 @@ The next part is an array of builtin command names, followed by an array of thei
 
 /*Array of pointers pointing to builtin shell functions*/
 int (*shell_array_pointing_builtin_functions[]) (char **) = {
-  &lsh_cd,
-  &lsh_help,
-  &lsh_exit
+  &shell_builtin_command_cd,
+  &shell_builtin_command_help,
+  &shell_builtin_command_exit
 };
 
 int count_shell_number_builtins() {
@@ -56,7 +56,7 @@ int count_shell_number_builtins() {
    @param args List of args.  args[0] is "cd".  args[1] is the directory.
    @return Always returns 1, to continue executing.
  */
-int lsh_cd(char **args)
+int shell_builtin_command_cd(char **args)
 {
   if (args[1] == NULL) {
     fprintf(stderr, "lsh: expected argument to \"cd\"\n");
@@ -73,7 +73,7 @@ int lsh_cd(char **args)
    @param args List of args.  Not examined.
    @return Always returns 1, to continue executing.
  */
-int lsh_help(char **args)
+int shell_builtin_command_help(char **args)
 {
   int i;
   printf("Public Domain Shell by a cooked egg\n");
@@ -93,7 +93,7 @@ int lsh_help(char **args)
    @param args List of args.  Not examined.
    @return Always returns 0, to terminate execution.
  */
-int lsh_exit(char **args)
+int shell_builtin_command_exit(char **args)
 {
   return 0;
 }
