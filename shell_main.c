9a1e21c4fc79ecb1b76f96a32734bea52592c25f
BoQsc <BoQsc@users.noreply.github.com>
2021-10-06 16:59:36 +0300

    Update shell_main.c

--- shell_main.c
+++ shell_main.c
@@ -98,35 +98,6 @@ int shell_builtin_command_exit(char **args)
   return 0;
 }
 
-/**
-  @brief Launch a program and wait for it to terminate.
-  @param args Null terminated list of arguments (including program).
-  @return Always returns 1, to continue execution.
- */
-int lsh_launch(char **args)
-{
-  pid_t pid, wpid;
-  int status;
-
-  pid = fork();
-  if (pid == 0) {
-    // Child process
-    if (execvp(args[0], args) == -1) {
-      perror("lsh");
-    }
-    exit(EXIT_FAILURE);
-  } else if (pid < 0) {
-    // Error forking
-    perror("lsh");
-  } else {
-    // Parent process
-    do {
-      wpid = waitpid(pid, &status, WUNTRACED);
-    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-  }
-
-  return 1;
-}
 
 #include "shell_main_execute.c"
 #include "shell_main_read_command_line.c"
