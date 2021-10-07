0bce110c0da4de98555fa710d08de00b32ac9a10
BoQsc <BoQsc@users.noreply.github.com>
2021-10-07 10:50:10 +0300

    Update shell_register_command.c

--- shell_register_command.c
+++ shell_register_command.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 #define shell_register_command(command_name) add_command(#command_name)
 void add_command(char* command_name){
-	 printf("Command to be registered: %s", command_name);
+    printf("Command to be registered: %s", command_name);
+	char *last = strrchr(command_name, '_');
+	if (last != NULL) {
+		printf("\nLast token: '%s'\n", last+1);
+	}
+
 	
 }
 
@@ -10,4 +16,8 @@ void add_command(char* command_name){
 int main()
     {
     shell_register_command(shell_command_cd);
+	
+
+	
+	
 }
