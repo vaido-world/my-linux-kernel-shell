30f899e252189877a612d663aedabd481a1c1ae8
BoQsc <BoQsc@users.noreply.github.com>
2021-10-07 14:23:55 +0300

    Update shell_register_command.c
    Enables tokenization by string instead of character
    Replaces strchr with strstr

--- shell_register_command.c
+++ shell_register_command.c
@@ -5,9 +5,9 @@
 void add_command(char* command_name){
     printf("Command to be registered: %s", command_name);
 	
-	char *last_token = strrchr(command_name, '_');
+	char *last_token = strstr(command_name, "command_");
 	if (last_token != NULL) {
-		printf("\nLast token: '%s'\n", last_token+1);
+		printf("\nLast token: '%s'\n", last_token+strlen("command_"));
 	}
 	
 	int command_pointer_address = &command_name;
@@ -25,7 +25,7 @@ void shell_command_cd(){
 int main()
     {
 	// shell_convert_builtin_function_to_command
-    shell_register_command(shell_command_cd);
+    shell_register_command(shell_builtin_command_cd_test);
     shell_register_command(shell_command_goto);
 	shell_register_command(shell_command_gotos);
 
