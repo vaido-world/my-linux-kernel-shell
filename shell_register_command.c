602079458fa557c45e69fd676bb6c2b1d5e72e0a
BoQsc <BoQsc@users.noreply.github.com>
2021-10-08 10:35:07 +0300

    Update shell_register_command.c

--- shell_register_command.c
+++ shell_register_command.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
+
+char *shell_builtin_command_list[];
+int (*shell_array_pointing_builtin_functions[]) (char **);
+
+int count_shell_number_builtins() {
+  return sizeof(shell_builtin_command_list) / sizeof(char *);
+}
+
+
 #define shell_register_command(command_name) add_command(#command_name)
 void add_command(char* command_name){
     printf("Command to be registered: %s", command_name);
+
+
+	int i;
+	int index = 0;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (someCondition)
+		{
+			someArray[index++] = someValue;
+		}
+	}
+
+
+	shell_array_pointing_builtin_functions
 	
 	char *last_token = strstr(command_name, "command_");
 	if (last_token != NULL) {
 		printf("\nLast token: '%s'\n", last_token+strlen("command_"));
 	}
-	
-	int command_pointer_address = &command_name;
-	printf("\nIs the function declared?: '%i'\n", command_pointer_address);
 	
 }
 
@@ -25,7 +46,7 @@ void shell_command_cd(){
 int main()
     {
 	// shell_convert_builtin_function_to_command
-    shell_register_command(shell_builtin_command_cd_test);
+    shell_register_command(shell_builtin_command_cd);
     shell_register_command(shell_command_goto);
 	shell_register_command(shell_command_gotos);
 
