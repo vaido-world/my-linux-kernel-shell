4b8183c2f24e6d388c07a3b48789955494d251d1
BoQsc <BoQsc@users.noreply.github.com>
2021-10-08 21:38:17 +0300

    Update shell_main.c

--- shell_main.c
+++ shell_main.c
@@ -40,6 +40,7 @@ The next part is an array of builtin command names, followed by an array of thei
 // https://stackoverflow.com/questions/252748/how-can-i-use-an-array-of-function-pointers
 
 // https://stackoverflow.com/questions/5488608/how-define-an-array-of-function-pointers-in-c/5488718#5488718
+// https://www.ibm.com/docs/en/zos/2.2.0?topic=functions-pointers
 int (*shell_array_pointing_builtin_functions[]) (char **) = {
   &shell_builtin_command_cd,
   &shell_builtin_command_help,
