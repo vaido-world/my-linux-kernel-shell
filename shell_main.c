cece8b8651009af9a8d6a1207d2c96831472ac0e
BoQsc <BoQsc@users.noreply.github.com>
2021-10-10 19:18:15 +0300

    Update shell_main.c

--- shell_main.c
+++ shell_main.c
@@ -44,6 +44,7 @@ The next part is an array of builtin command names, followed by an array of thei
 // https://stackoverflow.com/questions/840501/how-do-function-pointers-in-c-work
 // https://www.cprogramming.com/tutorial/function-pointers.html
 // https://www.youtube.com/watch?v=p4sDgQ-jao4
+// https://www.youtube.com/watch?v=sZ8GJ1TiMdk
 
 
 // Points to char array and returns int
