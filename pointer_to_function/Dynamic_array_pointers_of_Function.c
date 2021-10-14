24424e6607ea600e0d22bbbdb47fa81515c28443
BoQsc <BoQsc@users.noreply.github.com>
2021-10-14 11:50:18 +0300

    Update Dynamic_array_pointers_of_Function.c

--- pointer_to_function/Dynamic_array_pointers_of_Function.c
+++ pointer_to_function/Dynamic_array_pointers_of_Function.c
@@ -27,7 +27,7 @@ int functions_names_array_capacity_in_elements = 0;
 int functions_names_array_capacity_in_bytes = 0;
 int functions_names_array_initial_size = 0;  
 
-void add_function_to_heap_dynamic_array(void * function_address){
+void add_function_call_to_heap_dynamic_array(void * function_address){
 	functions_calls_array_capacity_in_elements++;
 	functions_calls_array_capacity_in_bytes = functions_calls_array_capacity_in_elements * sizeof(functionType);
 	
@@ -46,8 +46,8 @@ int main() {
 
   functions_calls_heap_dynamic_array = malloc(functions_calls_array_capacity_in_bytes);  // heap dynamic array, instead of stack static array; 
   
-  add_function_to_heap_dynamic_array(&declared_function);
-  add_function_to_heap_dynamic_array(&declared_function2);
+  add_function_call_to_heap_dynamic_array(&declared_function);
+  add_function_call_to_heap_dynamic_array(&declared_function2);
   
   functions_calls_heap_dynamic_array[0]("asd");
   functions_calls_heap_dynamic_array[1]("asd");
