439f534723b4adc1f3c48c0bcf8475bc8db9cab4
BoQsc <BoQsc@users.noreply.github.com>
2021-10-14 10:01:17 +0300

    Update Dynamic_array_pointers_of_Function.c

--- pointer_to_function/Dynamic_array_pointers_of_Function.c
+++ pointer_to_function/Dynamic_array_pointers_of_Function.c
@@ -2,47 +2,30 @@
 // TODO: https://levelup.gitconnected.com/alternatives-to-global-variables-34982becfcc
 // tcc -b -run Dynamic_array_pointers_of_Function.c
 #include <stdio.h>
-void declared_function(char *name){
-	printf("\nI'm declared function");
-
-}
-
-void declared_function2(char *name){
-	printf("\nI'm Second declared function");
-
-}
-
-
-
 #include <stdlib.h>
 
+void declared_function(char *args){
+	printf("I'm declared function\n");
 
-typedef void (*functionType)(char *);           
-                          
-  // https://stackoverflow.com/questions/572547/what-does-static-mean-in-c
-  int static capacity = 0;     // initial capacity
-  int static initial_size = 0;  // initial size
-  functionType static *functions;
+}
 
+void declared_function2(char *args){
+	printf("I'm Second declared function \n");
+
+}
+
+
+typedef void (*functionType)(char *);  
+functionType static *functions;                              
+int static array_capacity = 0; // initial capacity
+int static initial_size = 0;   // initial size
+// https://stackoverflow.com/questions/572547/what-does-static-mean-in-c
 
 void add_dynamic_array(void * function_address){
-	
-
-	
-
-
-
-  // ...
-
-  // adding element and need more space
-
-	 capacity += 1;
-     functions = realloc(functions, capacity * sizeof(functionType));
-     functions[initial_size++] = function_address;
-
-  
-printf("\n array_capacity %i", capacity);
-
+	array_capacity += 1;
+    functions = realloc(functions, array_capacity * sizeof(functionType));
+    functions[initial_size++] = function_address;  
+	printf("array_capacity %i\n", array_capacity);
 	
 	
 }
@@ -50,10 +33,7 @@ printf("\n array_capacity %i", capacity);
 
 
 int main() {
-
-
-
-  functions = malloc(capacity*sizeof(functionType));  // heap dynamic array, instead of stack static array; // dynamic array
+  functions = malloc(array_capacity*sizeof(functionType));  // heap dynamic array, instead of stack static array; // dynamic array
   add_dynamic_array(&declared_function);
   add_dynamic_array(&declared_function2);
   functions[0]("asd");
