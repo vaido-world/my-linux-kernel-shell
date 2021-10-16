08d5b15c1cb0125c1642e4a113883dfe60798163
BoQsc <BoQsc@users.noreply.github.com>
2021-10-16 14:52:45 +0300

    Create heap_dynamic_array_strings.c

---
+++ dynamic_arrays/heap_dynamic_array_strings.c
@@ -0,0 +1,20 @@
+// tcc -b -run heap_dynamic_array_strings.c
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+	size_t termination_symbol_size = 1;
+	char * initialcharacters = "Carla_";
+	
+	string = malloc(strlen(initialcharacters) + termination_symbol_size);
+	strcpy(string, initialcharacters);
+	
+	char * newcharacters = "$$$$$";
+	
+	string = realloc(string, strlen(newcharacters) + strlen(string) + termination_symbol);
+	strcat(string, newcharacters);
+	
+	printf(string);
+	free(string);
+}
